65a336426892c41d5ed1671352498c57b54505b0
Mahmudjon-Asqarov <mahmidjonasqarov101@gmail.com>
2022-03-15 19:45:56 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  return (
+    <div className="app">
+      <h1>Apple iPhone 1</h1>
+      <h1>Apple iPhone 2</h1>
+      <h2>Apple iPhone 1</h2>
+      <h2>Apple iPhone 2</h2>
+      <h1>Apple iPhone 2</h1>
+    </div>
+  );
+}
+export default App;
